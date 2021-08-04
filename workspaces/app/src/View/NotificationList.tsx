c7a3b54e22cd27bfd5c905d7e6d88c20992ec300
allan-aimy <allan@aimy.io>
2021-08-04 15:35:17 +1200

    display notification craeted time(e.g 15 mins ago)

--- workspaces/app/src/View/NotificationList.tsx
+++ workspaces/app/src/View/NotificationList.tsx
@@ -38,7 +38,8 @@ export default function NotificationList() {
   const { notifications } = user;
 
   const renderNotification = ({ item: n }: { item: NotificationDTO }) => {
-    console.log(n);
+    const timeDiff = new Date().getTime() - n.createdTime;
+    const timeDiffStr = millisecondsToStr(timeDiff);
 
     return (
       <ListItem bottomDivider>
@@ -48,7 +49,7 @@ export default function NotificationList() {
               <Text style={{ color: `grey` }}>{n.subject}</Text>
             </View>
             <View style={{ flex: 1, flexDirection: `row-reverse` }}>
-              <Text style={{ color: `grey` }}>5 months ago</Text>
+              <Text style={{ color: `grey` }}>{timeDiffStr}</Text>
             </View>
           </View>
           <ListItem.Title>{n.content}</ListItem.Title>
@@ -87,3 +88,36 @@ const styles = StyleSheet.create({
     flexDirection: `row`,
   },
 });
+
+function millisecondsToStr(milliseconds: number) {
+  // TIP: to find current time in milliseconds, use:
+  // var  current_time_milliseconds = new Date().getTime();
+
+  function numberEnding(number: number) {
+    return number > 1 ? "s" : "";
+  }
+
+  var temp = Math.floor(milliseconds / 1000);
+  var years = Math.floor(temp / 31536000);
+  if (years) {
+    return years + " year" + numberEnding(years);
+  }
+  //TODO: Months! Maybe weeks?
+  var days = Math.floor((temp %= 31536000) / 86400);
+  if (days) {
+    return days + " day" + numberEnding(days);
+  }
+  var hours = Math.floor((temp %= 86400) / 3600);
+  if (hours) {
+    return hours + " hr" + numberEnding(hours);
+  }
+  var minutes = Math.floor((temp %= 3600) / 60);
+  if (minutes) {
+    return minutes + " min" + numberEnding(minutes);
+  }
+  var seconds = temp % 60;
+  if (seconds) {
+    return seconds + " sec" + numberEnding(seconds);
+  }
+  return "just now"; //'just now' //or other string you like;
+}
