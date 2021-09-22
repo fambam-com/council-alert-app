9502609886f7eca197546317d86f2f8e4d86de26
allan-aimy <allan@aimy.io>
2021-09-23 08:47:04 +1200

    add swipe action to list item

--- workspaces/app/src/View/NotificationList.tsx
+++ workspaces/app/src/View/NotificationList.tsx
@@ -1,18 +1,12 @@
 import React, { useRef, useState, useEffect, useContext } from "react";
-import {
-  View,
-  StyleSheet,
-  FlatList,
-  AppState,
-  TouchableHighlight,
-} from "react-native";
+import { View, StyleSheet, FlatList, AppState } from "react-native";
 import { ListItem, Text, Button, Badge } from "react-native-elements";
 import StateContext from "../Context";
 import { $post } from "../Util/Request";
 import { NotificationDTO } from "../../../server/src/util/DBOperator";
 import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
-import DetailModal from "./DetailModal";
+import NotificationRow from "./NotificationRow";
 
 export default function NotificationList() {
   const { id, notificationToken, user, setState, getNotification } =
@@ -90,44 +84,6 @@ export default function NotificationList() {
     });
   };
 
-  const renderNotification = ({ item: n }: { item: NotificationDTO }) => {
-    const timeDiff = new Date().getTime() - n.createdTime;
-    const timeDiffStr = millisecondsToStr(timeDiff);
-
-    const isUrgent = n.importance === `urgent`;
-
-    let subject = n.subject || ``;
-
-    if (isUrgent) {
-      subject = subject?.replace("(URGENT)", "");
-    }
-
-    return (
-      <TouchableHighlight onPress={() => onPressItem(n)}>
-        <ListItem bottomDivider>
-          <ListItem.Content>
-            <View style={styles.subtitleView}>
-              <View style={{ flex: 2, flexDirection: `row` }}>
-                <Text style={{ color: `grey` }}>{subject}</Text>
-                {isUrgent && (
-                  <Badge
-                    badgeStyle={{ marginLeft: 3 }}
-                    status="error"
-                    value="URGENT"
-                  />
-                )}
-              </View>
-              <View style={{ flex: 1, flexDirection: `row-reverse` }}>
-                <Text style={{ color: `grey` }}>{timeDiffStr}</Text>
-              </View>
-            </View>
-            <ListItem.Title>{n.content}</ListItem.Title>
-          </ListItem.Content>
-        </ListItem>
-      </TouchableHighlight>
-    );
-  };
-
   const _testBtn = () => {
     return (
       <View>
@@ -189,11 +145,11 @@ export default function NotificationList() {
       <FlatList
         keyExtractor={(n) => n._id.valueOf().toString()}
         data={notifications}
-        renderItem={renderNotification}
+        renderItem={NotificationRow}
         ListEmptyComponent={renderEmptyNotification()}
       ></FlatList>
 
-      <DetailModal
+      {/* <DetailModal
         visible={detailModalInfo.detailModalVisible}
         notification={detailModalInfo.notification}
         hideModal={() => {
@@ -202,7 +158,7 @@ export default function NotificationList() {
             notification: undefined,
           });
         }}
-      ></DetailModal>
+      ></DetailModal> */}
     </View>
   );
 }
@@ -214,41 +170,4 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
     padding: 24,
   },
-  subtitleView: {
-    justifyContent: `space-between`,
-    flexDirection: `row`,
-  },
 });
-
-function millisecondsToStr(milliseconds: number) {
-  // TIP: to find current time in milliseconds, use:
-  // var  current_time_milliseconds = new Date().getTime();
-
-  function numberEnding(number: number) {
-    return number > 1 ? "s" : "";
-  }
-
-  var temp = Math.floor(milliseconds / 1000);
-  var years = Math.floor(temp / 31536000);
-  if (years) {
-    return years + " year" + numberEnding(years);
-  }
-  //TODO: Months! Maybe weeks?
-  var days = Math.floor((temp %= 31536000) / 86400);
-  if (days) {
-    return days + " day" + numberEnding(days);
-  }
-  var hours = Math.floor((temp %= 86400) / 3600);
-  if (hours) {
-    return hours + " hr" + numberEnding(hours);
-  }
-  var minutes = Math.floor((temp %= 3600) / 60);
-  if (minutes) {
-    return minutes + " min" + numberEnding(minutes);
-  }
-  var seconds = temp % 60;
-  if (seconds) {
-    return seconds + " sec" + numberEnding(seconds);
-  }
-  return "just now"; //'just now' //or other string you like;
-}
