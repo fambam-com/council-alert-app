1c45b7166c944479a800e274e048d9ce1dd3ed59
allan-aimy <allan@aimy.io>
2021-09-28 09:10:11 +1300

    add some snooze options

--- workspaces/app/src/View/SnoozeModal.tsx
+++ workspaces/app/src/View/SnoozeModal.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
-import { View, StyleSheet } from "react-native";
-import { Button, Overlay, Text } from "react-native-elements";
+import { View, StyleSheet, TouchableHighlight, Alert } from "react-native";
+import { Button, Overlay, Text, Icon } from "react-native-elements";
 import StateContext from "../Context";
 import { $post } from "../Util/Request";
 import { NotificationDTO } from "../../../server/src/util/DBOperator";
@@ -21,10 +21,64 @@ export default function DetailModal({
     }
   }, [visible]);
 
+  const renderOption = (option: any) => {
+    const { name, icon } = option;
+
+    return (
+      <TouchableHighlight
+        activeOpacity={0.6}
+        underlayColor="#DDDDDD"
+        onPress={() =>
+          Alert.alert("info", "Message", [
+            {
+              text: "ok",
+              onPress: () => {
+                hideModal();
+
+                getNotification(id);
+              },
+            },
+          ])
+        }
+      >
+        <View
+          style={{
+            width: 100,
+            height: 70,
+            alignItems: `center`,
+            justifyContent: `center`,
+            // backgroundColor: `lightgrey`,
+            margin: 7,
+          }}
+        >
+          <Icon name={icon}></Icon>
+          <Text>{name}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  };
+
   return (
     <View>
-      <Overlay isVisible={!!visible} onBackdropPress={hideModal}>
-        <Text>{n?._id}</Text>
+      <Overlay
+        isVisible={!!visible}
+        onBackdropPress={hideModal}
+        overlayStyle={{ width: `100%`, maxWidth: 250 }}
+      >
+        <View style={{ flexDirection: `row`, flexWrap: `wrap` }}>
+          {renderOption({
+            name: `2 hours later`,
+            icon: `rowing`,
+          })}
+          {renderOption({
+            name: `4 hours later`,
+            icon: `rowing`,
+          })}
+          {renderOption({
+            name: `4 hours later`,
+            icon: `rowing`,
+          })}
+        </View>
       </Overlay>
     </View>
   );
